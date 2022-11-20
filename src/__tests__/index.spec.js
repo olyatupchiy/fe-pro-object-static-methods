2ccfb0db287c67f263fe7a3768309f12bfce9b7d
rammfall <15bogdan17@gmail.com>
2022-11-20 20:17:59 +0200

    Add tasks and tests

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -1,3 +1,41 @@
-test('Test', () => {
-  expect(false).toStrictEqual(false);
+import { entries, fromEntries, keys, values } from '../index';
+
+describe('All functions', function () {
+  describe('keys', function () {
+    it('should returns correct value', () => {
+      expect(keys({ name: 'name', value: 'value' })).toEqual(['name', 'value']);
+    });
+  });
+
+  describe('values', function () {
+    it('should returns correct value', () => {
+      expect(values({ name: 'name1', value: 'value1' })).toEqual([
+        'name1',
+        'value1',
+      ]);
+    });
+  });
+
+  describe('entries', function () {
+    it('should returns correct value', () => {
+      expect(entries({ name: 'name1', value: 'value1' })).toEqual([
+        ['name', 'name1'],
+        ['value', 'value1'],
+      ]);
+    });
+  });
+
+  describe('fromEntries', function () {
+    it('should returns correct value', () => {
+      expect(
+        fromEntries([
+          ['key', 'value'],
+          ['foo', 'bar'],
+        ])
+      ).toEqual({
+        key: 'value',
+        foo: 'bar',
+      });
+    });
+  });
 });
---
+++ src/index.js
@@ -0,0 +1,30 @@
+/**
+ * Принимает объект, возвращает массив названий свойств. Нельзя пользоваться
+ * встроенными статическими методами объекта, используйте цикл
+ * @param {Object} object
+ * @returns {string[]}
+ */
+export const keys = (object) => {};
+
+/**
+ * Принимает объект, возвращает массив значений объекта. Также без использования
+ * встроенных методов, юзаем цикл
+ * @param {Object} object
+ * @returns {any[]}
+ */
+export const values = (object) => {};
+
+/**
+ * Принимаем объект, возвращаем энтри. Все также без методов, используем цикл
+ * @param {Object} object
+ * @returns {[string, any][]}
+ */
+export const entries = (object) => {};
+
+/**
+ * Тут наоборот, мы должны сделать из энтри объект обратно) Можете погуглить
+ * оригинальный метод)
+ * @param {[string, any][]} entries
+ * @returns {Object}
+ */
+export const fromEntries = (entries) => {};
